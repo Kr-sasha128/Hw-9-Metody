81b2a9a4866069a1b4739ec3b450aba30f259c18
Alexander-PC <br128128@g,ail.com>
2024-12-02 16:14:12 +0300

    Сток Коммит

---
+++ src/Main.java
@@ -0,0 +1,53 @@
+import java.time.LocalDate;
+import java.util.Random;
+import java.util.random.RandomGenerator;
+
+import static java.lang.Math.random;
+
+public class Main {
+
+
+//    }
+
+
+    //    public static void printissues(int issuesCount) {
+//        System.out.println(issuesCount);
+//    }
+//
+//
+//    public static int sum(int[] nambers) {
+//        int sam = 0;
+//        for (int i = 0; i < nambers.length; i++) {
+//            sam = sam +  nambers [i];
+//        }
+//        return sam;
+//
+//    }
+//
+//
+//    public static void printSeparator() {
+//        System.out.println("+++++++++++++++++++");
+//        System.out.println("--------------------");
+//
+//
+//    }
+    public static void main(String[] args) {
+
+
+//        int[] issuesByMonths = {4, 6, 7, 9, 2, 5, 12, 3, 7, 10, 6, 7, 1, 8};
+//        printSeparator();
+//        for (int i = 0; i < issuesByMonths.length; i++) {
+//            printissues(issuesByMonths[i]);
+//            if ((i + 1) % 3 == 0) {
+//                printSeparator();
+//            }
+//        }
+//        printSeparator();
+//        int tonal = sum(issuesByMonths);
+//        printissues(tonal);
+
+
+//
+
+    }
+}
